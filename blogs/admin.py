ee1572554b8173b9238f9033e69b987f19adc796
Amey Gupta <ameygupta@Ameys-MacBook-Pro.local>
2026-02-16 02:51:41 +0530

    editables

--- blogs/admin.py
+++ blogs/admin.py
@@ -4,6 +4,9 @@ from .models import Blog
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'author', 'category', 'status', 'is_featured')
+    search_fields = ('title', 'id', 'category__category_name', 'status')
+    list_editable = ('is_featured',)
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Blog, BlogAdmin)
