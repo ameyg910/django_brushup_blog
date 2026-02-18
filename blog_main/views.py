b26e4285d2ae96b301c9bb175d07a74d48fbf7bf
Amey Gupta <ameygupta@Ameys-MacBook-Pro.local>
2026-02-18 14:40:54 +0530

    featured posts

--- blog_main/views.py
+++ blog_main/views.py
@@ -2,11 +2,13 @@
 
 from django.shortcuts import render
 
-from blogs.models import Category
+from blogs.models import Category, Blog
 
 def home(request):
     categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True).order_by('updated_at')
     context = {
         'categories': categories,
+        'featured_posts': featured_posts,
     }
     return render(request, 'home.html', context)
